bd05e8e33bce9d46c9f146ea77b90ae8fec64e79
Ethan Cornwell <ethan.aubrey22@gmail.com>
2022-11-16 11:37:41 -0700

    Added a load companies list

--- app.js
+++ app.js
@@ -1,11 +1,40 @@
 //Making a network call to the acme API
 let loadButton = document.querySelector('#loadButton');
 let list = document.querySelector('#usersList');
+let loadRandomButton = document.querySelector('#loadRandomButton');
+let h1 = document.querySelector('#randomH1');
+let loadCompanies = document.querySelector('#loadCompanies');
+let companiesList = document.querySelector('#companiesList');
+
 
 
 loadButton.addEventListener('click', function(ev){
     loadUsers();
 });
+
+loadRandomButton.addEventListener('click', function(){
+    loadRandomUser();
+});
+
+loadCompanies.addEventListener('click', function(){
+    loadCompaniesList();
+    console.log('loadcomp button works')
+});
+
+
+
+
+async function loadCompaniesList(){
+    let response = await fetch('https://www.acme-api.com/api/companies');
+    let data = await response.json();
+    //let companies = data[0].name;
+    for(let i = 0; i < data.length; i++){
+        let company = data[i];
+        let li = document.createElement('li');
+        list.appendChild(li);
+        li.innerText = company.name;
+    }  
+}
 
 async function loadUsers(){
     let response = await fetch('https://www.acme-api.com/api/users');
@@ -16,6 +45,11 @@ async function loadUsers(){
         let li = document.createElement('li');
         list.appendChild(li);
         li.innerText = user.fullName;
-    }
-    
+    }  
+}
+
+async function loadRandomUser(){
+    let response = await fetch('https://www.acme-api.com/api/users/random');
+    let user = await response.json();
+    randomH1.innerText = user.fullName;
 }
